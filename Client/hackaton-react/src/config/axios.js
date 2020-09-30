b0a4e93bd00c049d4d92aa019e80641c9b2130b5
Krum <krum.kereziev@mentormate.com>
2020-09-30 11:23:10 +0300

    removed foler src

--- Client/hackaton-react/src/config/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-//const { REACT_APP_API_URL } = process.env;
-
-export const httpClient = axios.create({
-  //baseURL: REACT_APP_API_URL,
-  withCredentials: true,
-});
