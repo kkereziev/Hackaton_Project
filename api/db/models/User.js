06523ef1b61dc3bf4f87b15e2a452786d8731bc6
Simeon Lazarov <simeon.lazarov@mentormate.com>
2020-09-30 16:39:10 +0300

    feat(User): Added User model

---
+++ api/db/models/User.js
@@ -0,0 +1,41 @@
+const { Model } = require('sequelize');
+const bcrypt = require('bcrypt');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    async matchPassword(password) {
+      return bcrypt.compare(password, this.phash);
+    }
+  }
+
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING(),
+        allowNull: false,
+        unique: true,
+        validate: {
+          min: 4,
+          max: 20,
+        },
+      },
+      phash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          min: 8,
+          max: 20,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      tableName: 'Users',
+    }
+  );
+
+  User.afterValidate((user) => {
+    user.phash = bcrypt.hashSync(user.phash, 12);
+  });
+  return User;
+};
