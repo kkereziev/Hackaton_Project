0d95dbab7e8a455455414565c100ce3ec88c990a
PetarParushev <lightone3591@gmail.com>
2020-09-30 15:42:15 +0300

    feat: add Login Page UI

---
+++ Client/hackaton-frontend/src/pages/Auth/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { MainCard } from "src/components/generic/styles/Containers";
+
+export const InnerContainer = styled(MainCard)`
+  padding: 20px;
+  margin-top: 150px;
+  width: 45%;
+  min-width: 300px;
+`;
