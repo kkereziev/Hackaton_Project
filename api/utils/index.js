507be805f114dca1e1d9752b413ffd2a770d462e
Krum <krum.kereziev@mentormate.com>
2020-10-01 15:27:10 +0300

    feat(validation): added validation

--- api/utils/index.js
+++ api/utils/index.js
@@ -3,5 +3,16 @@ const cookieExtractor = require('./cookieExtractor');
 const findUserByUsername = require('./helper/Auth/findUserByUsername');
 const { extractMondays, extractPertsOfDate, checkIfDateIsRight } = require('./helper/Timesheet');
 const passport = require('./auth');
+const { registerSchema, loginSchema } = require('./Validation/auth');
 
-module.exports = { cookieExtractor, findUserByUsername, checkIfDateIsRight, extractMondays, passport, jwt, extractPertsOfDate };
+module.exports = {
+  registerSchema,
+  loginSchema,
+  cookieExtractor,
+  findUserByUsername,
+  checkIfDateIsRight,
+  extractMondays,
+  passport,
+  jwt,
+  extractPertsOfDate,
+};
