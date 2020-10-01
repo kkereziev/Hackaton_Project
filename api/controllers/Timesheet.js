98a94c7333cd6f86b4e756f95413f99896441931
Simeon Lazarov <simeon.lazarov@mentormate.com>
2020-10-01 12:44:18 +0300

    feat(Timesheet): Creating timesheet

--- api/controllers/Timesheet.js
+++ api/controllers/Timesheet.js
@@ -1,139 +1,49 @@
+const date = require('date.js');
+const jwt = require('jsonwebtoken');
 const { models, Op } = require('../db/index');
-const { jwt, getCurrentUser, findUserById, findUserByUsername } = require('../utils/index');
-const config = require('../config/config');
+const { extractMondays } = require('../utils/index');
+const { secret } = require('../config/config');
+const cookieExtractor = require('../utils/cookieExtractor');
 
 const get = {
-  // users
-  async users(req, res, next) {
-    const users = await models.User.findAll({
-      include: [
-        { model: models.UserBio, as: 'Bio' },
-        { model: models.User, as: 'Follower' },
-        { model: models.User, as: 'Following' },
-        { model: models.Post, as: 'Posts' },
-      ],
-    }).catch(next);
-    res.send(users);
+  async allTimesheets(req, res, next) {
+    console.log(req.user.dataValues.id);
+    // req.user = decoded.user;
   },
-  // me
-  async currentUser(req, res, next) {
-    getCurrentUser(req)
-      .then((user) => {
-        res.send(user);
-      })
-      .catch(next);
+
+  async timesheet(req, res, next) {
+    console.log('timesheet');
   },
-  // {id}/posts
-  async userPosts(req, res, next) {
-    const userId = req.params.id;
-    const user = await findUserById(userId).catch(next);
-    res.send(user.Posts);
-  },
-  // {username}/exists
-  async usernameExists(req, res, next) {
-    const usernameToSearch = req.params.username;
-    const user = await findUserByUsername(usernameToSearch).catch(next);
-    if (user) {
-      res.json({ exists: true });
-      return;
-    }
-    res.json({ exists: false });
-  },
-  // users/{identifier}
-  async getUserByIdentifier(req, res, next) {
-    const identifier = req.params.identifier;
-    let user;
-    if (isNaN(identifier)) {
-      user = await findUserByUsername(identifier).catch(next);
-    } else {
-      user = await findUserById(identifier).catch(next);
-    }
-    res.send(user);
-  },
-  // {id}/followers
-  async followers(req, res, next) {
-    const userId = req.params.id;
-    const user = await findUserById(userId).catch(next);
-    res.send(user['Follower']);
-  },
-  // {id}/following
-  async following(req, res, next) {
-    const userId = req.params.id;
-    const user = await findUserById(userId).catch(next);
-    res.send(user['Following']);
+
+  getDates(req, res, next) {
+    const dates = extractMondays();
+    res.send({ dates });
   },
 };
 
 const post = {
-  //signup
-  async register(req, res, next) {
-    const { name, username, email, password } = req.body;
-    const newUser = await models.User.create({
-      name,
-      username,
-      email,
-      phash: password,
-    }).catch(next);
+  async createTimesheet(req, res, next) {
+    const { id } = req.user.dataValues;
+    const { startDate } = req.body;
+    const startingDate = new Date(startDate);
 
-    const userRole = await models.Role.findOne({
-      where: { name: 'user' },
-    }).catch(next);
+    const dates = extractMondays();
+    dates.map((date) => )
 
-    await newUser.setRoles([userRole.id]).catch(next);
-    res.send(newUser);
-  },
-  // signin
-  async login(req, res, next) {
-    const { username, password } = req.body;
-    const user = await findUserByUsername(username).catch(next);
-    const match = await user.matchPassword(password).catch(next);
-    if (!match) {
-      res.status(401).send('Invalid password');
-      return;
-    }
-    const token = jwt.createToken({ id: user.id });
-    res.cookie(config.authCookieName, token).send(user);
-  },
-  // signout
-  async signout(req, res, next) {
-    const token = req.cookies[config.authCookieName];
-
-    res.clearCookie(config.authCookieName).json({ success: true });
-  },
-  // {id}/follow
-  async follow(req, res, next) {
-    const userToFollowId = req.params.id;
-    const currentUser = await getCurrentUser(req).catch(next);
-    await models.UserFollowers.create({
-      followerId: currentUser.id,
-      followingId: userToFollowId,
-    }).catch(next);
-    res.json({ success: true });
-  },
-  //{id}/uinfollow
-  async unfollow(req, res, next) {
-    const userToUnfollowId = req.params.id;
-    const currentUser = await getCurrentUser(req).catch(next);
-
-    const connection = await models.UserFollowers.findOne({
-      where: {
-        [Op.and]: [{ followerId: currentUser.id }, { followingId: userToUnfollowId }],
-      },
-    }).catch(next);
-    await connection.destroy().catch(next);
-
-    res.json({ success: true });
+    //models.Timesheet.create();
   },
 };
 
 const patch = {
-  //users/me
-  async updateUser(req, res, next) {
-    const { caption, content, dateOfBirth, country, city, occupation } = req.body;
-    const currentUser = await getCurrentUser(req).catch(next);
-    await currentUser.createBio({ caption, content, dateOfBirth }).catch(next);
-    res.json({ message: 'Updated!' });
+  async updateTimesheet(req, res, next) {
+    console.log('updateTimesheet');
   },
 };
 
-module.exports = { get, post, patch };
+const remove = {
+  async deleteTimesheet(req, res, next) {
+    console.log('deleteTimesheet');
+  },
+};
+
+module.exports = { get, post, patch, remove };
--- api/utils/index.js
+++ api/utils/index.js
@@ -1,6 +1,7 @@
 const jwt = require('./jwt');
 const cookieExtractor = require('./cookieExtractor');
 const findUserByUsername = require('./helper/Auth/findUserByUsername');
+const extractMondays = require('./helper/Timesheet/extractMondays');
 const passport = require('./auth');
 
-module.exports = { cookieExtractor, findUserByUsername, passport, jwt };
+module.exports = { cookieExtractor, findUserByUsername, extractMondays, passport, jwt };
