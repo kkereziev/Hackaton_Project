b76144d6eda627bcf5efcc529e443dde2c522795
Stella Stoyneva <stella.stoyneva@mentormate.com>
2020-10-02 11:40:29 +0300

    feat: Reg and Login pages styled with new logo

--- Client/hackaton-frontend/src/pages/Auth/styles.js
+++ Client/hackaton-frontend/src/pages/Auth/styles.js
@@ -3,7 +3,12 @@ import { MainCard } from "src/components/generic/styles/Containers";
 
 export const InnerContainer = styled(MainCard)`
   padding: 20px;
-  margin-top: 150px;
-  width: 45%;
+  margin: 50px 10px;
+  width: 95%;
   min-width: 300px;
+  align-self: center;
+`;
+
+export const RegLogLinkSpan = styled.span`
+  margin-left: 15px;
 `;
