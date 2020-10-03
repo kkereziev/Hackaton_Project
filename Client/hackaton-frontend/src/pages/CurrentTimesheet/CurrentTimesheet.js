83aab695606ee53a2c63f27011b345647bbae1b3
PetarParushev <lightone3591@gmail.com>
2020-10-03 15:44:27 +0300

    fix: guard currentTimesheet route

--- Client/hackaton-frontend/src/pages/CurrentTimesheet/CurrentTimesheet.js
+++ Client/hackaton-frontend/src/pages/CurrentTimesheet/CurrentTimesheet.js
@@ -2,17 +2,19 @@ import React, { useState, useEffect } from "react";
 import { Title } from "src/components/generic/styles/Title";
 import { Table } from "src/components/generic/Table/Table";
 import { TitleWithBtnsDiv } from "src/components/generic/styles/Containers";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { getCurrentTimesheet } from "src/api_endpoints/timesheets";
 
 export const CurrentTimesheet = () => {
   const { name } = useParams();
+  const history = useHistory();
   const [timesheet, setTimesheet] = useState();
 
   useEffect(() => {
     const fetchRows = async () => {
       try {
         const timesheet = await getCurrentTimesheet(name);
+        if (timesheet === null) history.push("/dashboard");
         console.log(timesheet);
         setTimesheet(timesheet);
       } catch (error) {}
